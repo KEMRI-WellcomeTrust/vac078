a4765bc638c3875cbefdef15561c6de5418ff75c
MwangiMuiruri <84372660+MwangiMuiruri@users.noreply.github.com>
2024-01-05 09:17:16 +0300

    05Jan2024_1

--- app.R
+++ app.R
@@ -404,12 +404,13 @@ queries<-queries_now|>
 
 
 
-in_completed1<- suppressWarnings(fread(here("study data/Visit Completed v_s SDV_2.csv"),
-                                       colClasses = c("text","numeric","text","text","text","text","text","text","text","text","text","text","text","text","text","text","text","text","text","text","text","text")))|>fill_blanks()|>
+in_completed1<- suppressWarnings(fread(here("study data/Visit Completed v_s SDV_2.csv")))|>fill_blanks()|>
+# ,
+#                                        colClasses = c("text","numeric","text","date","text","text","text","date","text","text","text","text","text","text","text","text","text","text","text","text","text","text")))|>fill_blanks()|>
   # mutate(VISDAT=as.Date(VISDAT,"%d %m %Y"),
   #        `Entered Date-Time`=as.Date(`Entered Date-Time`, "%d %m %Y %H:%M"))
-  mutate(VISDAT=as.Date(VISDAT,"%d %b %y"),
-         `Entered Date-Time`=as.Date(`Entered Date-Time`,"%d %b %y" ))#"%d %m %Y %H:%M"
+  mutate(VISDAT=as.Date(VISDAT,"%d-%b-%Y"),
+         `Entered Date-Time`=as.Date(`Entered Date-Time`,"%d-%b-%y" ))#"%d %m %Y %H:%M"
 in_completed<-in_completed1|>
   dplyr::rename(status="EntryStatus")|>
   filter(status == "Incomplete")|>
@@ -1060,7 +1061,7 @@ cleanmeds<-left_join(medications|>
                               `Last Data Time`=as.Date(`Last Data Time`, "%d-%b-%Y %H:%M")),
                      durta<-left_join(dateofvisit|>
                                         dplyr::rename(VISDAT=`Visit date`),
-                                      in_completed1|>
+                                      incomp<-in_completed1|>
                                         dplyr::rename(Subject="Subject Id", VISIT=VISITNAME)|>mutate(VISDAT=as.Date(VISDAT, "%Y-%m-%d"))|>filter(VISIT%nin%c("Day 14","Day 42","Day 70","Day 140","Day 196","Day 292","Day 348","Day 404","B 14","B 84","B 140","B 224","B 280","B 336")),by=c("Subject","VISIT","VISDAT"),relationship = "many-to-many")|>
                        filter(EntryStatus!="Not Started")|>
                        select(2:3,18,23:26)|>
